e1f16f543249f939a6bca3d4ab0c498095fbd0cf
N3hc <chenpingguo3@gmail.com>
2025-01-30 19:38:19 +0100

    Carusel de categorias/expansiones

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -10,57 +10,50 @@ import { CardsApiService } from '../../services/api/cards-api.service';
 })
 export class CarrouselComponent implements OnInit {
   sets: any[] = [];
-  itemWidth: number = 0;
-  translateX: number = 0;
-  dataItems = [1, 3, 5, 6];
+  currentIndex = 0;
+  index = 0;
 
   constructor(private cardsApiService: CardsApiService) {}
 
   ngOnInit(): void {
     this.loadSets();
-    this.updateItemWidth();
   }
-
-  @HostListener('window:resize')
-  onResize() {
-    this.updateItemWidth();
-  }
-
-  updateItemWidth() {
-    const screenWidth = window.innerWidth;
-    let itemsPerView = this.dataItems[0];
-    if (screenWidth >= 1200) itemsPerView = this.dataItems[3];
-    else if (screenWidth >= 992) itemsPerView = this.dataItems[2];
-    else if (screenWidth >= 768) itemsPerView = this.dataItems[1];
-
-    this.itemWidth = 100 / itemsPerView;
-  }
-
-  move(direction: number) {
-    const totalItems = this.sets.length;
-    const itemsPerView = 100 / this.itemWidth;
-    const maxTranslate = (totalItems * this.itemWidth) - 100;
-
-    this.translateX += direction * this.itemWidth;
-
-    if (this.translateX < 0) {
-      this.translateX = maxTranslate;
-    } else if (this.translateX > maxTranslate) {
-      this.translateX = 0;
-
-    }
-  }
-
 
   loadSets(): void {
     this.cardsApiService.getPokemonAllSets().subscribe({
       next: (sets) => {
-        this.sets = sets.data.slice(0, 10);;
-        console.log(this.sets); 
+        this.sets = this.shuffleArray(sets.data);
       },
       error: (error) => {
         console.error('Error al cargar los sets:', error);
       }
     });
+  }
+
+  move(direction: number): void {
+    const totalItems = this.sets.length;
+    this.currentIndex = (this.currentIndex + direction + totalItems) % totalItems; 
+  }
+
+  getVisibleIndices(): number[] {
+    const totalItems = this.sets.length;
+    const visibleIndices = [];
+    for (let i = -3; i <= 3; i++) {
+      const index = (this.currentIndex + i + totalItems) % totalItems;
+      visibleIndices.push(index);
+    }
+    return visibleIndices;
+  }
+
+  trackByIndex(index: number): number {
+    return index;
+  }
+
+  shuffleArray<T>(array: T[]): T[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [array[i], array[j]] = [array[j], array[i]]; 
+    }
+    return array; 
   }
 }
--- src/app/pages/items/products/products.component.ts
+++ src/app/pages/items/products/products.component.ts
@@ -1,37 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
-import { CardsApiService } from '../../../services/api/cards-api.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
-
-  cartas: any[] = []; 
-  sets: any[] = [];
-
-  constructor(private cardsApiService: CardsApiService) { }
-
-  ngOnInit(): void {
-    this.cargarSets();
-  }
-
-
-  cargarSets(): void {
-    this.cardsApiService.getPokemonAllSets().subscribe({
-      next: (sets) => {
-        this.sets = sets.data; 
-        console.log(sets.data); 
-      },
-      error: (error) => {
-        console.error('Error al cargar los sets:', error);
-      }
-    });
-  }
+export class ProductsComponent {
 
 
 }
