0b4a3ea67bed294573afbb95990b7dc26fcf4185
ArduiP <162721278+ArduiP@users.noreply.github.com>
2025-02-21 18:41:21 +0100

    azsd

--- src/app/components/carrousel-highlights/carrousel-highlights.component.ts
+++ src/app/components/carrousel-highlights/carrousel-highlights.component.ts
@@ -12,24 +12,37 @@ import { SearchService } from '../../services/search/search.service';
 export class CarrouselHighlightsComponent {
 
   sets:any [] = [];
+  series: string[] = [];
 
     constructor(private cardsApiService: CardsApiService
     ) {}
 
-  loadSets(): void {
-    this.cardsApiService.getPokemonAllSets().subscribe({
-      next: (sets) => {;
-        this.sets = sets.data;
-        console.log(sets)
-      },
-      error: (error) => {
-        console.error('Error al cargar los sets:', error);
-      }
-    });
-  }
+    loadSets(): void {
 
-  ngOnInit(): void {
-    this.loadSets();
-  }
+      this.cardsApiService.getPokemonAllSets().subscribe({
+        next: (sets) => {
+          const set = sets.data;
+
+          this.sets = set;
+
+          // Usar un Set para obtener valores únicos
+          const seriesSet = new Set<string>();
+          this.sets.forEach((set) => seriesSet.add(set.series));
+
+          // Convertir el Set de nuevo a un array
+          this.series = Array.from(seriesSet);
+
+          console.log(sets);
+          console.log(this.series);
+        },
+        error: (error) => {
+          console.error('Error al cargar los sets:', error);
+        }
+      });
+    }
+
+    ngOnInit(): void {
+      this.loadSets();
+    }
 
 }
