7d0c70867bf106029e87fed66c4478d815c01f1a
ArduiP <162721278+ArduiP@users.noreply.github.com>
2025-03-05 20:05:57 +0100

    Component Product-Carrousel Día 5/03/2024

--- src/app/components/product-carrousel/product-carrousel.component.ts
+++ src/app/components/product-carrousel/product-carrousel.component.ts
@@ -6,47 +6,61 @@ import { Card } from '../../interfaces/carrousel.interface';
   standalone: true,
   imports: [],
   templateUrl: './product-carrousel.component.html',
-  styleUrl: './product-carrousel.component.css'
+  styleUrls: ['./product-carrousel.component.css']
 })
 export class ProductCarrouselComponent {
-  products: any[] = [];
+  anterior: number = 0;
+  actual: number = 1;
+  siguiente: number = 2;
 
- Cards: Card[] = [
-  {
-    id: 'dp5-98',
-    name: 'Glaceon',
-    image: 'https://images.pokemontcg.io/dp5/98.png',
-    setName: 'Majestic Dawn'
-  },
-  {
-    id:'sv2-222',
-    name: 'Tyranitar',
-    image:"https://images.pokemontcg.io/sv2/222.png",
-    setName: 'Paldea Evolved'
-  },
-  {
-    id:'pl2-109',
-    name:"Luxray GL",
-    image:"https://images.pokemontcg.io/pl2/109.png",
-    setName: 'Rising Rivals'
-  },
-  {
-    id:"sm75-78",
-    name:"ultra Necrozma GX",
-    image:"https://images.pokemontcg.io/sm75/78.png",
-    setName:"Dragon Majesty"
-  },
-  {
-    id:"sm12-249",
-    name:"Venusaur & Snivy-GX",
-    image:"https://images.pokemontcg.io/sm12/249.png",
-    setName:"Cosmic Eclipse"
-  },
-  {
-    id:"sm6-146",
-    name:"Unit Energy FightingDarknessFairy",
-    image:"https://images.pokemontcg.io/sm6/146.png",
-    setName:"Forbidden Light"
+  Cards: Card[] = [
+    {
+      id: 'dp5-98',
+      name: 'Glaceon',
+      image: 'https://images.pokemontcg.io/dp5/98.png',
+      setName: 'Majestic Dawn'
+    },
+    {
+      id: 'sv2-222',
+      name: 'Tyranitar',
+      image: 'https://images.pokemontcg.io/sv2/222.png',
+      setName: 'Paldea Evolved'
+    },
+    {
+      id: 'pl2-109',
+      name: 'Luxray GL',
+      image: 'https://images.pokemontcg.io/pl2/109.png',
+      setName: 'Rising Rivals'
+    },
+    {
+      id: 'sm75-78',
+      name: 'Ultra Necrozma GX',
+      image: 'https://images.pokemontcg.io/sm75/78.png',
+      setName: 'Dragon Majesty'
+    },
+    {
+      id: 'sm12-249',
+      name: 'Venusaur & Snivy-GX',
+      image: 'https://images.pokemontcg.io/sm12/249.png',
+      setName: 'Cosmic Eclipse'
+    },
+    {
+      id: 'sm6-146',
+      name: 'Unit Energy FightingDarknessFairy',
+      image: 'https://images.pokemontcg.io/sm6/146.png',
+      setName: 'Forbidden Light'
+    }
+  ];
+
+  siguienteProducto() {
+    this.anterior = this.actual;
+    this.actual = this.siguiente;
+    this.siguiente = (this.siguiente + 1) % this.Cards.length;
   }
- ];
+
+  anteriorProducto() {
+    this.siguiente = this.actual;
+    this.actual = this.anterior;
+    this.anterior = (this.anterior - 1 + this.Cards.length) % this.Cards.length;
+  }
 }
--- src/app/interfaces/carrousel.interface.ts
+++ src/app/interfaces/carrousel.interface.ts
@@ -1,4 +1,5 @@
 export interface Card {
+  [index: number]: any;
   id: string;
   name: string,
   image: string,
