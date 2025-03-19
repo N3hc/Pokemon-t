4a9bdb3fbd0168758b0afdf1ca425b888ca5f773
N3hc <chenpingguo3@gmail.com>
2025-03-19 19:38:25 +0100

    Router Refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/auth/login/login.component';
-import { RegisterComponent } from './pages/auth/register/register.component';
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, LoginComponent,RegisterComponent],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Pokemon-t';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
-import { HomeComponent } from './pages/home/home.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { DetailComponent } from './pages/items/detail/detail.component';
-import { ResultsComponent } from './pages/items/results/results.component';
 
-export const appRoutes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'cart', component: CartComponent },
-    { path: 'detail', component: DetailComponent },
-    { path: 'results', component: ResultsComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
-
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: 'home' }
 ];
